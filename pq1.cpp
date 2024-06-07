9409393d2baf00a23e407ab9ce169837b6728cce
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-06-07 15:25:44 -0700

    modified pq1.cpp

--- pq1.cpp
+++ pq1.cpp
@@ -3,51 +3,36 @@
 #include <iostream>
 
 using namespace std;
-
 int main()
 {
-   int month, day, year;
-   int month_plus_day = month + day;
-
-
-   cout << "Enter a month in numeric form: " << endl;
-   cin >> month;
-   
-   if (0 >= month || month > 12)
-   {
-    cout << "Month invalid" << endl;
-   }
-   else
-   {
-    cout << "Enter a day: " << endl;
-    cin >> day;
     
-    if (0 >= day || day >= 31)
-    {
-        cout << "Invalid day" << endl;
-    }
-    else
-    {
-        cout << "Enter a two digit year: " << endl;
-        cin >> year;
+    int num_days;
+    int total = 5;
+    int min_value = 1;
+    int count;
+    int salary = 1;
 
-        if (year > 100)
-        {
-            cout << "Invalid year" << endl;
-        }
-        else
-        {
-            if (month_plus_day == year)
-            {
-                cout << "The date of " << month << "/" << day << "/" << year << " is magic!" << endl;
-            }
-            else
-            {
-                cout << "The date of " << month << "/" << day << "/" << year << " is not magic!" << endl;
-            }
-        }
+    cout << "Enter the number of days: ";
+    cin >> num_days;
+
+    while (num_days < min_value)
+    {
+        cout << "The number of days must be at least one" << endl;
+        cout << "Enter the number of days: ";
+        cin >> num_days;
     }
-   }
+
+    while (num_days > 0)
+    {
+        cout << "Day " << min_value++ << "\t$" << salary << endl;
+
+        total += salary;
+        salary *= 2;
+        num_days--;
+    }
+
+    cout << "Total = " << total << endl;
+
 
     return 0;
 }
