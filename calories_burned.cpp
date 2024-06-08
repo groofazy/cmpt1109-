114d8618ba9cf35a570d11b7e741258ff7bdc085
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-06-07 18:00:29 -0700

    modified calories_burned.cpp

---
+++ calories_burned.cpp
@@ -0,0 +1,20 @@
+// Running on a particular treadmill you burn 3.6 calories per minute. Write a program that uses a loop to display the number of calories burned after 5, 10, 15, 20, 25, and 30 minutes.
+
+#include <iostream>
+
+int main()
+{
+
+    double burned_cals = 3.6;
+    int min_intervals = 5;
+    int count;
+
+    for (count = 1; count < 5; count++)
+    {
+        cout << "Minute: " << count << 
+    }
+
+
+
+    return 0;
+}
