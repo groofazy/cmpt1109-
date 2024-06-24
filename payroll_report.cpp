ae788a3e0ddd1d51f994d0bb83d36d19985149cf
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-06-23 20:48:07 -0700

    modified payroll_report.exe

--- payroll_report.cpp
+++ payroll_report.cpp
@@ -7,23 +7,20 @@ int main()
     int emp_num;
     double gross, state, federal, fica;
 
-    cout << "Employee Number";
+    cout << "Employee Number: ";
     cin >> emp_num;
-    cout << "Gross Pay";
+    cout << "Gross Pay: ";
     cin >> gross;
-    cout << "State Tax";
+    cout << "State Tax: ";
     cin >> state;
-    cout << "Federal Tax";
+    cout << "Federal Tax: ";
     cin >> federal;
-    cout << "FICA withholdings:";
+    cout << "FICA withholdings: ";
     cin >> fica;
 
     cout << "Employee Totals\n";
-    cout << "---------------";
-    cout << "Gross Pay: " << gross << "\t" << "State Tax: " << state << "\t" << "Federal Tax" << "\t" << "FICA withholdings" << fica;
-
-    
-
+    cout << "---------------\n";
+    cout << "Gross Pay: " << gross << "\t" << "State Tax: " << state << "\t" << "Federal Tax: " << federal << "\t" << "FICA withholdings: " << fica;
 
     return 0;
 }
