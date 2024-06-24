1aa5bd60f330a21c412db7d0d28f18afb9514025
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-06-23 21:06:05 -0700

    payroll

--- payroll_report.cpp
+++ payroll_report.cpp
@@ -5,22 +5,47 @@ using namespace std;
 int main()
 {
     int emp_num;
-    double gross, state, federal, fica;
+    double gross, state, federal, fica, net;
 
+    do {
     cout << "Employee Number: ";
     cin >> emp_num;
+
+
     cout << "Gross Pay: ";
     cin >> gross;
+    
+    
+    
     cout << "State Tax: ";
     cin >> state;
+    
+    
+    
     cout << "Federal Tax: ";
     cin >> federal;
+    
+    
+    
     cout << "FICA withholdings: ";
     cin >> fica;
 
+    if (emp_num < 0 || gross < 0)
+    {
+        cout << "Negative values are invalid, please enter positive value";
+        cin >> gross;
+    }
+
+    } while (emp_num != 0);
+
+
+
+
+
+    cout << "\n";
     cout << "Employee Totals\n";
     cout << "---------------\n";
-    cout << "Gross Pay: " << gross << "\t" << "State Tax: " << state << "\t" << "Federal Tax: " << federal << "\t" << "FICA withholdings: " << fica;
+    cout << "Gross Pay: " << gross << "\t" << "State Tax: " << state << "\t" << "Federal Tax: " << federal << "\t" << "FICA withholdings: " << fica << "Net Pay: " << net;
 
     return 0;
 }
