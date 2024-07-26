3a52f836264eb73ae1dcf088a74434d93d39a45a
Ryan Gamboa <gabe.gamboa05@gmail.com>
2024-07-25 23:48:48 -0700

    modify

--- str_selectsort.cpp
+++ str_selectsort.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <string>
-
-void selectionSort(string[], int);
-void showArray(string[], int);
-
 using namespace std;
+
+void showArray(string[], int);
+void selectionSort(string[], int);
+
 int main()
 {
     const int SIZE = 10;
-    string arr[SIZE] = {"Toji", "Nobara", "Yuta", "Chiikawa", "Hachiware", "Usagi", "Denji", "Shinji", "Megumi", "Yoshi"};
+    string arr[SIZE] = { "Toji", "Nobara", "Yuta", "Chiikawa", "Hachiware", "Usagi", "Denji", "Shinji", "Megumi", "Yoshi" };
 
     cout << "This is the string unsorted\n";
+    showArray(arr, SIZE);
+    cout << endl;
+    cout << "This is the string sorted in Alphabeltical Order\n";
+    selectionSort(arr, SIZE);
     showArray(arr, SIZE);
 
     return 0;
@@ -21,7 +25,7 @@ void selectionSort(string arr[], int size)
     int minIndex;
     string minVal;
 
-    for(int start = 0; start < (size - 1); start++) // 
+    for (int start = 0; start < (size - 1); start++) // 
     {
         minIndex = start; // setting index value to 0
         minVal = arr[start]; // lowest val word assigned at arr[0]
@@ -30,17 +34,17 @@ void selectionSort(string arr[], int size)
             if (arr[index] < minVal) // if instance to set new minVal
             {
                 minVal = arr[index]; // init minVal to newest low val
-                minIndex = index; // lowest index = index of 
+                minIndex = index; // lowest index = index of newest low val
             }
         }
-        swap(arr[minIndex], arr[start]); // swap 
+        swap(arr[minIndex], arr[start]); // swap new low val with previous low val
     }
 }
 
 void showArray(string arr[], int size)
 {
-    for(int index = 0; index < size; index++)
+    for (int index = 0; index < size; index++)
     {
-        cout << arr[index] << ", ";
+        cout << arr[index] << endl;
     }
 }
